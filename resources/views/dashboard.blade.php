65e974aab49d1aa8f10bfe0967df74d131f66be3
k-slugger <shubhamghosalkar.sg@gmail.com>
2017-06-27 02:04:07 +0530

    Create dashboard layout

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="#">
+                <div class="form-group">
+                    <textarea class="form-control" rows="5" placeholder="Your Post" name="new-post" id="new-post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>Okay okay yeah yeah!</p>
+                <div class="info">
+                    Posted by Shubham on 27 Jun 2017
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Okay okay yeah yeah!</p>
+                <div class="info">
+                    Posted by Shubham on 27 Jun 2017
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
